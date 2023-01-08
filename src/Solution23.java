20378951452f871f0275444771bd857638c61022
ryandingzy <ryandingzy@gmail.com>
2023-01-08 00:23:47 -0800

    The count of subarrays whose sum is dividable by k

--- src/Solution23.java
+++ src/Solution23.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution23 {
+    // 子序列之和整除，HashMap
+    public int subarraysDivByK(int[] nums, int k) {
+        Map<Integer, Integer> record = new HashMap<Integer, Integer>();
+        record.put(0, 1);
+        int sum = 0, ans = 0;
+        for (int elem : nums) {
+            sum += elem;
+            // 注意 Java 取模的特殊性，当被除数为负数时取模结果为负数，需要纠正
+            int modulus = (sum % k + k) % k;
+            int same = record.getOrDefault(modulus, 0);
+            ans += same;
+            record.put(modulus, same + 1);
+        }
+        return ans;
+    }
+
 }
